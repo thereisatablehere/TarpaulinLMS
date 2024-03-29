c0ce6051fb840cc0428b33413554501059c2072d
RERICKSON002 <rerickson002@vplincsci017.ad.csbsju.edu>
2024-03-28 23:15:12 -0500

    Added sample records into insertDatatest.sql

--- DBscr/insertDataTest.sql
+++ DBscr/insertDataTest.sql
@@ -20,6 +20,102 @@ ALTER TABLE tarp_watches DISABLE CONSTRAINT FK_TARP_WATCHES3;
 ALTER TABLE tarp_rates DISABLE CONSTRAINT FK_TARP_RATES1;
 ALTER TABLE tarp_rates DISABLE CONSTRAINT FK_TARP_RATES2;
 
+--potential data
+
+--INSERT INTO tarp_student (username, f_name, l_name, password) VALUES ('erickson25', 'Rodney', 'Erickson', 'password1');
+--INSERT INTO tarp_student (username, f_name, l_name, password) VALUES ('maahs35', 'Keiley', 'Maahs', 'password2');
+--INSERT INTO tarp_student (username, f_name, l_name, password) VALUES ('davis12', 'Carson', 'Davis', 'password3');
+--INSERT INTO tarp_student (username, f_name, l_name, password) VALUES ('grove5', 'Carter', 'Grove', 'password4');
+--INSERT INTO tarp_student (username, f_name, l_name, password) VALUES ('bartness2', 'Dylan', 'Bartness', 'password5');
+--
+--
+--INSERT INTO tarp_instructor (username, f_name, l_name, password) VALUES('imad2000', 'Imad', 'Rahal', 'password6');
+--INSERT INTO tarp_instructor (username, f_name, l_name, password) VALUES('john2', 'John', 'Davidson', 'password7');
+--INSERT INTO tarp_instructor (username, f_name, l_name, password) VALUES('bill34', 'Bill', 'Billy', 'password8');
+--INSERT INTO tarp_instructor (username, f_name, l_name, password) VALUES('erin1234', 'Erin', 'Bert', 'password9');
+--
+--
+--INSERT INTO tarp_community (community_id, username) VALUES ('csbsju', 'davis12');
+--INSERT INTO tarp_community (community_id, username) VALUES ('MN', 'maahs35');
+--
+--
+--INSERT INTO tarp_course (course_id, username) VALUES ('CSCI101', 'imad2000');
+--INSERT INTO tarp_course (course_id, username) VALUES ('CSCI331', 'imad2000');
+--INSERT INTO tarp_course (course_id, username) VALUES ('MATH101', 'bill34');
+--INSERT INTO tarp_course (course_id, username) VALUES ('MATH231', 'bill34');
+--INSERT INTO tarp_course (course_id, username) VALUES ('THEO111', 'erin1234');
+--
+--
+--INSERT INTO tarp_s_comment (course_id, s_username, comment, cdate) VALUES ('CSCI101', 'grove5', 'Great course!', TIMESTAMP '2024-03-28 10:30:00');
+--INSERT INTO tarp_s_comment (course_id, s_username, comment, cdate) VALUES ('CSCI101', 'erickson25', 'Awesome course!', TIMESTAMP '2024-05-28 10:30:00');
+--
+--
+--INSERT INTO tarp_i_comment (course_id, s_username, comment, cdate) VALUES ('CSCI101', 'imad2000', 'Cool course!', TIMESTAMP '2024-02-28 10:30:00');
+--INSERT INTO tarp_i_comment (course_id, s_username, comment, cdate) VALUES ('MATH101', 'bill34', 'Good job everyone!!', TIMESTAMP '2024-05-28 10:30:00');
+--
+--
+--INSERT INTO tarp_lecture (lecture_id, course_id, length, url) VALUES ('Lecture 1', 'CSCI101', 20, 'https://example.com/lecture1/CSCI101');
+--INSERT INTO tarp_lecture (lecture_id, course_id, length, url) VALUES ('Lecture 2', 'CSCI101', 30, 'https://example.com/lecture2/CSCI101');
+--INSERT INTO tarp_lecture (lecture_id, course_id, length, url) VALUES ('Lecture 1', 'MATH101', 25, 'https://example.com/lecture1/MATH101');
+--INSERT INTO tarp_lecture (lecture_id, course_id, length, url) VALUES ('Lecture 2', 'MATH101', 15, 'https://example.com/lecture2/MATH101');
+--INSERT INTO tarp_lecture (lecture_id, course_id, length, url) VALUES ('Lecture 1', 'THEO111', 20, 'https://example.com/lecture1/THEO111');
+--
+--
+--INSERT INTO tarp_test (test_id, course_id) VALUES ('test 1', 'CSCI101');
+--INSERT INTO tarp_test (test_id, course_id) VALUES ('test 2', 'CSCI101');
+--INSERT INTO tarp_test (test_id, course_id) VALUES ('test 3', 'CSCI101');
+--INSERT INTO tarp_test (test_id, course_id) VALUES ('test 1', 'THEO111');
+--INSERT INTO tarp_test (test_id, course_id) VALUES ('test 2', 'THEO111');
+--INSERT INTO tarp_test (test_id, course_id) VALUES ('test 1', 'MATH101');
+--INSERT INTO tarp_test (test_id, course_id) VALUES ('test 2', 'MATH101');
+--
+--
+--INSERT INTO tarp_question (prompt, a, b, c, d, answer, test_id, course_id) VALUES ('What is 3 + 1?', '3', '4', '5', '6', 'A', 'test 1', 'MATH101');
+--INSERT INTO tarp_question (prompt, a, b, c, d, answer, test_id, course_id) VALUES ('What is 3 * 4?', '13', '14', '25', '12', 'D', 'test 1', 'MATH101');
+--INSERT INTO tarp_question (prompt, a, b, c, d, answer, test_id, course_id) VALUES ('What is 3 - 3?', '3', '14', '0', '6', 'C', 'test 1', 'MATH101');
+--INSERT INTO tarp_question (prompt, a, b, c, d, answer, test_id, course_id) VALUES ('What is 3 / 1?', '33', '4', '3', '6', 'C', 'test 1', 'MATH101');
+--
+--
+--INSERT INTO tarp_joined_by (community_id, username, rank) VALUES ('csbsju', 'davis12', 1);
+--INSERT INTO tarp_joined_by (community_id, username, rank) VALUES ('csbsju', 'maahs35', 2);
+--INSERT INTO tarp_joined_by (community_id, username, rank) VALUES ('csbsju', 'grove5', 3);
+--INSERT INTO tarp_joined_by (community_id, username, rank) VALUES ('csbsju', 'bartness2', 4);
+--INSERT INTO tarp_joined_by (community_id, username, rank) VALUES ('csbsju', 'erickson25', 5);
+--
+--
+--INSERT INTO tarp_taken_by (test_id, course_id, username, score) VALUES ('test 1', 'MATH101', 'grove5', 90);
+--INSERT INTO tarp_taken_by (test_id, course_id, username, score) VALUES ('test 1', 'MATH101', 'maahs35', 88);
+--INSERT INTO tarp_taken_by (test_id, course_id, username, score) VALUES ('test 1', 'MATH101', 'davis12', 65);
+--INSERT INTO tarp_taken_by (test_id, course_id, username, score) VALUES ('test 1', 'MATH101', 'bartness2', 95);
+--INSERT INTO tarp_taken_by (test_id, course_id, username, score) VALUES ('test 1', 'MATH101', 'erickson25', 80);
+--
+--
+--INSERT INTO tarp_enrolls (course_id, username) VALUES ('MATH101', 'grove5');
+--INSERT INTO tarp_enrolls (course_id, username) VALUES ('MATH101', 'maahs35');
+--INSERT INTO tarp_enrolls (course_id, username) VALUES ('MATH101', 'davis12');
+--INSERT INTO tarp_enrolls (course_id, username) VALUES ('MATH101', 'bartness2');
+--INSERT INTO tarp_enrolls (course_id, username) VALUES ('MATH101', 'erickson25');
+--
+--
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 1', 'MATH101', 'bartness2', true);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 2', 'MATH101', 'bartness2', true);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 1', 'MATH101', 'grove5', true);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 2', 'MATH101', 'grove5', false);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 1', 'MATH101', 'maahs35', true);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 2', 'MATH101', 'maahs35', false);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 1', 'MATH101', 'davis12', true);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 2', 'MATH101', 'davis12', false);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 1', 'MATH101', 'erickson25', false);
+--INSERT INTO tarp_watches (lecture_id, course_id, username, completed) VALUES ('Lecture 2', 'MATH101', 'erickson25', false);
+--
+--
+--INSERT INTO tarp_rates (s_username, i_username, instructor_score) VALUES ('davis12', 'bill34', 4);
+--INSERT INTO tarp_rates (s_username, i_username, instructor_score) VALUES ('erickson25', 'bill34', 7);
+--INSERT INTO tarp_rates (s_username, i_username, instructor_score) VALUES ('maahs35', 'bill34', 10);
+
+
+
+
 
 
 --INSERT INTO PRODUCTDEALS_SALESREP (slsrep_number, last, first, street, city, state, zip, commission_rate) VALUES (3.0, 'Jones', 'Mary', '123 Main', 'Grant', 'MI', '49219', 0.05);
