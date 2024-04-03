2750e225adcd6b245bbbe6551cc48b6e09b10232
thereisatablehere <dbartness001@csbsju.edu>
2024-04-03 05:47:38 -0500

    Student courses list

---
+++ Website/Scripts/loadSidebar.js
@@ -0,0 +1,26 @@
+const sidebar = `
+<nav class="sidebar">
+      <img class="close" src="../Images/close.svg">
+
+      <a>
+        <img src="../Images/news-paper.svg">
+        <p>Grade Report</p>
+      </a>
+
+      <a href="enrolledCoursesList.html">
+        <img src="../Images/education.svg">
+        <p>My Courses</p>
+      </a>
+
+      <a>
+        <img src="../Images/user-group.svg">
+        <p>My Communities</p>
+      </a>
+      
+      <a>
+        <img src="../Images/add-outline.svg">
+        <p>Create a Community</p>
+      </a>
+    </nav>`;
+
+document.write(sidebar);
