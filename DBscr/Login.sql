a1981bb718b1dd5244e4449b15c1bb70c9c8269e
Your Name <you@example.com>
2024-04-03 21:28:23 -0500

    commented routines

--- DBscr/Login.sql
+++ DBscr/Login.sql
@@ -1,11 +1,19 @@
+--DESCRIPTION: A function to determine if a user successfully logged in given username 
+--and password. Returns 1 if successful, 0 otherwise.
+
+--CODE
 Create or Replace Function Login(p_username varchar, p_password varchar) Return int
     AS
     toReturn int := 0;
     i_exists int := 0;
     s_exists int := 0;
     Begin
-        Select Count(*) into i_exists from TARP_INSTRUCTOR T where T.username = p_username and T.password = p_password;
-        Select Count(*) into s_exists from TARP_STUDENT T where T.username = p_username and T.password = p_password;
+        Select Count(*) into i_exists from TARP_INSTRUCTOR T 
+        where T.username = p_username and T.password = p_password;
+        
+        Select Count(*) into s_exists from TARP_STUDENT T 
+        where T.username = p_username and T.password = p_password;
+        
         If (i_exists = 1 OR s_exists = 1) Then
             toReturn := 1;
         End If;
