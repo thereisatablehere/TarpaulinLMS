4581f0d6a91d509a6b516f7f0b2322566efb30ca
thereisatablehere <125605662+thereisatablehere@users.noreply.github.com>
2024-04-24 21:24:44 -0500

    Fix Login.sql

--- DBscr/Login.sql
+++ DBscr/Login.sql
@@ -2,7 +2,7 @@
 --and password. Returns 1 if successful, 0 otherwise.
 
 --CODE
-Create or Replace Function Login(p_username varchar, p_password varchar) Return int
+create or replace Function Login(p_username varchar, p_password varchar) Return int
     AS
     toReturn int := 0;
     i_exists int := 0;
@@ -10,15 +10,17 @@ Create or Replace Function Login(p_username varchar, p_password varchar) Return 
     Begin
         Select Count(*) into i_exists from TARP_INSTRUCTOR T 
         where T.username = p_username and T.password = p_password;
-        
+
         Select Count(*) into s_exists from TARP_STUDENT T 
         where T.username = p_username and T.password = p_password;
-        
+
         IF(i_exists = 1) THEN
             toReturn := 1;
-        ELSE
-            toReturn := 2;
         END IF;
         
+        IF(s_exists = 1) THEN
+            toReturn := 2;
+        END IF;
+
         Return toReturn;
-    End ;
+    End;
