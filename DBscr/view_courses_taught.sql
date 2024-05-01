517f25b02b11f7711c4a2e6dbe176192de9e9189
thereisatablehere <125605662+thereisatablehere@users.noreply.github.com>
2024-04-30 20:19:56 -0500

    Update view_courses_taught.sql

--- DBscr/view_courses_taught.sql
+++ DBscr/view_courses_taught.sql
@@ -4,16 +4,16 @@
 
 -- CODE
 CREATE OR REPLACE VIEW view_courses_taught AS
-SELECT course_id
+SELECT course_id, username
 FROM TARP_COURSE tp
 ORDER BY COURSE_ID;
 
 --OUTPUT
 
---COURSE_ID           
-----------------------
---CSCI101
---CSCI331
---MATH101
---MATH231
---THEO111
+--COURSE_ID            USERNAME
+---------------------- --------
+--CSCI101              imad2000
+--CSCI331              imad2000
+--MATH101                bill34
+--MATH231                bill34
+--THEO111              erin1234
